6cfa6989c468051d667547bfb272fe016ebc7f49
Stephane Branly <stephanebranly.pro@gmail.com>
2020-11-27 11:13:58 +0100

    finalisation exo 32

--- td9/evenement.h
+++ td9/evenement.h
@@ -10,7 +10,8 @@ namespace TIME{
         Date date;
         std::string sujet;
     public:
-        Evt1j(const Date& d,const std::string& s):date(d),sujet(s){}
+        Evt1j(const Date& d,const std::string& s):date(d),sujet(s){  std::cout << "\nConstruction Evt1j : " << this << "\n";}
+         ~Evt1j(){ std::cout << "\nDestruction Evt1j : " << this << "\n";}
         const std::string& getDescription() const{return sujet; }
         const Date& getDate() const {return date; }
         void afficher(std::ostream& f= std::cout) const{
@@ -24,7 +25,8 @@ namespace TIME{
         Horaire debut;
         Duree duree;
     public:
-        Evt1jDur(const Date& d, const std::string& s, const Horaire& h, const Duree& dur):Evt1j(d,s),debut(h),duree(dur){};
+        Evt1jDur(const Date& d, const std::string& s, const Horaire& h, const Duree& dur):Evt1j(d,s),debut(h),duree(dur){  std::cout << "\nConstruction Evt1jDurv : " << this << "\n";};
+        ~Evt1jDur(){ std::cout << "\nDestruction Evt1jDur : " << this << "\n";}
         const Horaire& getHoraire() const { return debut; }
         const Duree& getDuree() const { return duree; }
         void afficher(std::ostream& f= std::cout) const {
@@ -37,7 +39,11 @@ namespace TIME{
         std::string lieu;
         std::string personnes;
     public:
-        Rdv(const Date& d, const std::string& s, const Horaire& h, const Duree& dur, const std::string& l, const std::string& p ):Evt1jDur(d,s,h,dur),lieu(l),personnes(p){};
+        Rdv(const Date& d, const std::string& s, const Horaire& h, const Duree& dur, const std::string& l, const std::string& p ):Evt1jDur(d,s,h,dur),lieu(l),personnes(p)
+        {
+            std::cout << "\nConstruction Rdv : " << this << "\n";
+        };
+        ~Rdv(){ std::cout << "\nDestruction Rdv : " << this << "\n";}
         const std::string& getPersonnes() const { return personnes ;}
         const std::string& getLieu() const { return lieu ; }
         void afficher(std::ostream& f= std::cout) const {
