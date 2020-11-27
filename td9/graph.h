cb9d42df989fb1a4306797534aef7dacaffebda5
Stephane Branly <stephanebranly.pro@gmail.com>
2020-11-27 08:48:20 +0100

    init td9

---
+++ td9/graph.h
@@ -0,0 +1,38 @@
+#if !defined(_GRAPH_H)
+#define _GRAPH_H
+#include<string>
+#include<stdexcept>
+using namespace std;
+class GraphException :public exception
+{
+    string info;
+public:
+    GraphException(const string& i) noexcept :info(i){}
+    virtual~GraphException() noexcept {}
+    const char* what() const noexcept {return info.c_str();
+                                      }
+};
+
+
+#include<list>
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Graph {
+    vector<list<unsigned int> > adj;
+    string name;
+public:
+    Graph(const string& n,unsigned int nb);
+    const string& getName() const;
+    unsigned int getNbVertices() const;
+    unsigned int getNbEdges() const;
+    void addEdge(unsigned int i,unsigned int j);
+    void removeEdge(unsigned int i,unsigned int j);
+    const list<unsigned int>& getSuccessors(unsigned int i) const;
+    const list<unsigned int> getPredecessors(unsigned int i) const;
+};
+ostream&operator<<(ostream& f,const Graph& G);
+
+#endif
